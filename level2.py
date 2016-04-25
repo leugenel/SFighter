dbb5ea75826fc18c8a1fdaa59a88aa7eca79ff6d
eugenel <leugenel@gmail.com>
2016-04-25 18:23:36 +0300

    add improve to level2

--- level2.py
+++ level2.py
@@ -12,54 +12,68 @@ num_iterations = 10
 number_bids = 10
 num_shares = 400
 sell_list = {}
-#price_list = []
 
-while True:
+def buy_shares(buy_this_value):
+    buy_done = False
     # Try buy 400 shares
-    price = Common.price_loop(sleep_time, num_iterations)
-    response, result = quoteRest.set_order(config.venue, config.stock, config.account, price, num_shares)
+    the_price = Common.price_loop(sleep_time, num_iterations)
+    response, result = quoteRest.set_order(config.venue, config.stock, config.account, the_price, buy_this_value)
     if response != 200:
         Common.plog_info(response)
         sys.exit()
     Common.plog_info("Set order for buy:")
     Common.plog_info(result)
-
     # Verify that buying works
-    buy_done=False
     for i in range (1, num_iterations):
         if quoteRest.get_order_status(config.venue, config.stock, result['id']):
             buy_done=True
             break
         time.sleep(sleep_time)
-
     # Cancel order if not buy it
     if not buy_done:
         quoteRest.cancel_order(config.venue, config.stock, result['id'])
         Common.plog_info("Cancel buy order")
-        # Lets try new buying
-        continue
 
-    #Now selling
+    return buy_done, the_price, result['id']
+
+def sell_shares(sell_this_value, price):
+    sell_done = False
     update_price = 100
     price_delta = 10
-    sell_done = False
     for j in range (1, num_iterations):
         update_price -= price_delta
-        response, result = quoteRest.set_order(config.venue, config.stock, config.account, price+update_price, num_shares, "sell")
+        response, result = quoteRest.set_order(config.venue, config.stock, config.account, price+update_price, sell_this_value, "sell")
         if response != 200:
             Common.plog_info(response)
             sys.exit()
         Common.plog_info("Set order for sell:")
         Common.plog_info(result)
-        if quoteRest.get_order_status(config.venue, config.stock, result['id']):
+        if quoteRest.get_order_status(config.venue, config.stock, share_id):
             sell_done = True
             Common.plog_info("Sell done for price "+str(price+update_price))
             break
         time.sleep(sleep_time)
     if not sell_done:
-        sell_list[result['id']]=price+update_price
+        Common.plog_info("Not sell this package")
+        #sell_list[share_id]=price+update_price
     Common.plog_info("Sell list:")
     Common.plog_info(sell_list)
 
+    return sell_done
 
+is_sel = True
+while True:
+    # Try buy 400 shares
+    price_buy = 0
+    if is_sel:
+        is_buy, price_buy, share_id = buy_shares(num_shares)
+        if not is_buy:
+            Common.plog_info("Not success to buy this package")
+            continue
+
+    #Now selling
+    if price_buy > 0:
+        is_sel=sell_shares(num_shares, price_buy)
+    else:
+        Common.plog_info("Price for buying is 0")
 
